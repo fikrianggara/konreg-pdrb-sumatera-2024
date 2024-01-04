9218843b648ee2e1968d86ae872a619f3ee6956b
fikrianggara <fikriseptriananggara@gmail.com>
2024-01-04 15:18:53 +0700

    initiate project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-screen bg-cyan-800 flex m-auto">
+      <h1 className="text-green-500 font-bold m-auto text-2xl">Hello world</h1>
+    </div>
+  );
+}
