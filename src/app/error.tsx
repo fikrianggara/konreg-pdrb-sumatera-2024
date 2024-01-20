7c8d086013fe0fee408e59aa77bb3a9e5b927546
fikrianggara <fikriseptriananggara@gmail.com>
2024-01-20 20:04:45 +0700

    add server error, not found error and loading page

---
+++ src/app/error.tsx
@@ -0,0 +1,41 @@
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+import Image from "next/image";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="h-96 w-full text-center flex flex-col m-auto justify-center space-y-16">
+      <h2 className="text-xl font-bold text-red-600 text-gray-600">
+        Oops! Terjadi Error. maaf yah :(
+      </h2>
+      <Image
+        src={"/assets/500.png"}
+        alt="error illustration"
+        width={500}
+        height={500}
+        className="self-center opacity-60"
+      />
+      <button
+        className="text-lg text-cyan-800 hover:underline"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,15 @@
+import { LoadingOverlay } from "@mantine/core";
 import React from "react";
 
 export default function loading() {
-  return <div>loading...</div>;
+  return (
+    <div className="h-96 w-full">
+      <LoadingOverlay
+        visible={true}
+        zIndex={1000}
+        overlayProps={{ radius: "sm", blur: 2 }}
+        loaderProps={{ color: "teal", type: "bars" }}
+      />
+    </div>
+  );
 }
