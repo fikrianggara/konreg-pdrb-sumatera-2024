ffa5ab4d4d08f8987f358ccdb253a049c3dcb92f
fikrianggara <fikriseptriananggara@gmail.com>
2024-05-29 04:26:31 +0700

    fix build time error

--- src/app/galeri/page.tsx
+++ src/app/galeri/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <div className="h-64 m-auto items-center text-center justify-center">
       {datas.length > 0 ? (
-        datas.map((d) => <div>{d.toString()}</div>)
+        datas.map((d, i) => <div key={i}>{d.toString()}</div>)
       ) : (
         <div className="h-full flex m-auto justify-center">
           <h1 className="text-xl text-gray-400 self-center">
