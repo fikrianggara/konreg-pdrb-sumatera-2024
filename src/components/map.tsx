c65121eb6247269cf81bcc20bbfd7e441aec90e7
fikrianggara <fikriseptriananggara@gmail.com>
2024-06-03 17:01:17 +0700

    add animation on maps component

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,9 +1,10 @@
 import { useWindowSize } from "@/hooks/useWindowSize";
 import { IconMapPinFilled } from "@tabler/icons-react";
 import mapboxgl from "mapbox-gl";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import MapboxMap, {
   FullscreenControl,
+  MapRef,
   Marker,
   NavigationControl,
   Popup,
@@ -14,26 +15,64 @@ const TOKEN =
   "pk.eyJ1IjoiZmlrcmlhbmdnYXJhIiwiYSI6ImNscmsyYnZveTA5NmUya29qZ2djYmU5YnkifQ.vVYOAwSJaUdd5gFA0TnxZA";
 
 const STYLE_URL = "mapbox://styles/fikrianggara/clrp5me4p009b01qy68ss74ks";
+const INITIAL_VIEW_PORT = {
+  longitude: 97.802708,
+  latitude: 0.928,
+  zoom: 3.4,
+  bearing: 0,
+  pitch: 7.5,
+};
+const MOBILE_VIEW_PORT = {
+  longitude: 98.612352,
+  latitude: 3.577446,
+  zoom: 5,
+  pitch: 20,
+  bearing: -20,
+};
+const VIEW_PORT = {
+  longitude: 95.802708,
+  latitude: -0.928,
+  zoom: 5.4,
+  bearing: 0,
+  pitch: 40.0,
+};
 export function Map3D() {
-  const { width } = useWindowSize();
   const [popupInfo, setPopupInfo] = useState<any>(null);
-  const [viewPort, setViewPort] = useState({
-    longitude: 95.802708,
-    latitude: -1.928,
-    zoom: 5.4,
-    bearing: -10,
-    pitch: 45.0,
-  });
+
+  const { width } = useWindowSize();
+
+  const mapRef = useRef<MapRef>(null);
+
+  useEffect(() => {
+    if (width && width < 800) {
+      setTimeout(() => {
+        mapRef.current?.flyTo({
+          ...MOBILE_VIEW_PORT,
+          center: [MOBILE_VIEW_PORT.longitude, MOBILE_VIEW_PORT.latitude],
+          duration: 5000,
+        });
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        mapRef.current?.flyTo({
+          ...VIEW_PORT,
+          center: [VIEW_PORT.longitude, VIEW_PORT.latitude],
+          duration: 5000,
+        });
+      }, 1000);
+    }
+  }, [width]);
   return (
     <div className="w-full h-fit rounded-xl flex items-center justify-center h-screen">
       <MapboxMap
         mapboxAccessToken={TOKEN}
-        initialViewState={viewPort}
+        initialViewState={INITIAL_VIEW_PORT}
+        ref={mapRef}
         mapStyle={STYLE_URL}
-        interactive={true}
-        onMove={(evt) => setViewPort(evt.viewState)}
-        trackResize
-        style={{ width: "100%", height: "100%" }}
+        // interactive={true}
+        // onMove={(evt) => setViewPort(evt.viewState)}
+        // trackResize
+        // style={{ width: "100%", height: "100%" }}
       ></MapboxMap>
     </div>
   );
@@ -80,12 +119,15 @@ function Map() {
     pitch: 50,
   });
   const [popupInfo, setPopupInfo] = useState<any>(null);
+  const mapRef = useRef<MapRef>();
 
   return (
     <div className="w-full h-full rounded-lg pb-8">
       <MapboxMap
+        // ref={mapRef}
         mapboxAccessToken={TOKEN}
         initialViewState={viewPort}
+        // initialViewState={INITIAL_VIEW_STATE}
         mapStyle="mapbox://styles/mapbox/streets-v12"
         interactive={true}
         onMove={(evt) => setViewPort(evt.viewState)}
