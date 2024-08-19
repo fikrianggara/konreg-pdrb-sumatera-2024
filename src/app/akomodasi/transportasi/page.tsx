bd959740149931e702b7213e3bb4992e7e5dea2b
Thoriq Ibadurrohman <thoriq312@gmail.com>
2024-08-19 21:38:25 +0700

    add tentang page and fix transportation page

--- src/app/akomodasi/transportasi/page.tsx
+++ src/app/akomodasi/transportasi/page.tsx
@@ -1,67 +1,85 @@
 "use client";
 // import { Span } from "next/dist/trace";
 import akomodasi from "@/data/akomodasi.json";
-import {
-  IconBrandApple,
-  IconBrandGooglePlay,
-  IconBrandWhatsapp,
-} from "@tabler/icons-react";
-import { Carousel } from "@mantine/carousel";
+import { IconBrandApple, IconBrandGooglePlay } from "@tabler/icons-react";
 
 function Page() {
-  const transportasi = akomodasi.transportasi;
+  const transportasi = akomodasi.transportasi.filter(
+    (t) => t.jenis == "online"
+  );
+  const rental = akomodasi.transportasi.filter((t) => t.jenis == "rental");
   const pic = akomodasi.pic;
 
   return (
     <div>
-      <h1 className="md:text-2xl font-bold">
-        Informasi Transportasi di Kota Jambi
-      </h1>
-      <hr />
-      <p className="my-4">
-        Kami merekomendasikan Anda untuk menggunakan aplikasi berikut:
-      </p>
+      <div>
+        <h1 className="md:text-2xl font-bold">Transportasi Online</h1>
+        <hr />
+        <p className="my-4">
+          Kami merekomendasikan Anda untuk menggunakan aplikasi berikut:
+        </p>
 
-      <div className="p-4 w-full flex justify-center	items-center">
-        <div className="p-4 w-3/4 flex flex-col md:flex-row justify-center items-center">
-          {transportasi.map((t, i) => (
-            <div className="rounded-lg bg-white h-fit shadow-lg m-4" key={i}>
-              <img
-                src={t.link_foto}
-                alt={t.nama}
-                className="w-full object-cover rounded-t-lg"
-              />
-              <div className="p-4 space-y-2 md:space-y-6">
-                <h2 className="md:text-xl font-medium text-center">{t.nama}</h2>
-                <ul className="w-full space-y-4 flex flex-wrap text-xs md:text-sm">
-                  <a
-                    href={t.link_android}
-                    className="flex items-center cursor-pointer hover:opacity-75 shadow-lg py-1 bg-green-500 text-white rounded-full w-full text-center text-ellipsis justify-center"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <IconBrandGooglePlay className="animate-bounce" />
-                    Unduh
-                  </a>
-                  <a
-                    href={t.link_apple}
-                    className="flex items-center cursor-pointer hover:opacity-75 shadow-lg py-1 bg-gray-500 text-white rounded-full w-full text-center text-ellipsis justify-center"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <IconBrandApple className="animate-bounce" />
-                    Unduh
-                  </a>
-                </ul>
+        <div className="p-4 w-full flex justify-center	items-center">
+          <div className="p-4 w-3/4 flex flex-col md:flex-row justify-center items-center">
+            {transportasi.map((t, i) => (
+              <div className="rounded-lg bg-white h-fit shadow-lg m-4" key={i}>
+                <img
+                  src={t.link_foto}
+                  alt={t.nama}
+                  className="w-full object-cover rounded-t-lg"
+                />
+                <div className="p-4 space-y-2 md:space-y-6">
+                  <h2 className="md:text-xl font-medium text-center">
+                    {t.nama}
+                  </h2>
+                  <ul className="w-full space-y-4 flex flex-wrap text-xs md:text-sm">
+                    <a
+                      href={t.link_android}
+                      className="flex items-center cursor-pointer hover:opacity-75 shadow-lg py-1 bg-green-500 text-white rounded-full w-full text-center text-ellipsis justify-center"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <IconBrandGooglePlay className="animate-bounce" />
+                      Unduh
+                    </a>
+                    <a
+                      href={t.link_apple}
+                      className="flex items-center cursor-pointer hover:opacity-75 shadow-lg py-1 bg-gray-500 text-white rounded-full w-full text-center text-ellipsis justify-center"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <IconBrandApple className="animate-bounce" />
+                      Unduh
+                    </a>
+                  </ul>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-      <p>
-        sebagai sarana transportasi di area Kota Jambi selama kegiatan
-        berlangsung.
-      </p>
+
+      <div>
+        <h1 className="md:text-2xl font-bold">Jasa Rental Mobil & Bus</h1>
+        <hr />
+        <table className="w-full text-left rtl:text-right text-gray-500 dark:text-gray-400 mt-4">
+          <thead className="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th>Perusahaan Rental</th>
+              <th>Alamat</th>
+              <th>Kontak</th>
+            </tr>
+          </thead>
+
+          {rental.map((p, i) => (
+            <tr key={i}>
+              <td className="font-bold py-4">{p.nama}</td>
+              <td>{p.alamat}</td>
+              <td>{p.telepon}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
 
       {/* <h1 className="md:text-2xl font-bold mt-4">PIC Penjemputan di Bandara</h1>
       <hr />
--- src/app/tentang/page.tsx
+++ src/app/tentang/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Breadcrumb from "@/components/breadcrumb";
 import Link from "next/link";
+import Image from "next/image";
 
 const page = () => {
   return (
@@ -11,7 +12,56 @@ const page = () => {
           <Link href="/kontak">Tentang</Link>
         </h1>
         <p className=" text-xs md:text-sm lg:text-base">
-          Sekilas tentang Konreg PDRB Regional Sumatera 2024
+          Sekilas tentang Konreg PDRB-ISE Se-Sumatera 2024
+        </p>
+        <Image src="/assets/KONREG.png" alt="logo" width={700} height={100} />
+        <p className=" text-xs md:text-sm lg:text-base text-justify">
+          Kegiatan Konreg PDRB-ISE Se-Sumatera atau Kegiatan Konsultasi Regional
+          Produk Domestik Regional Bruto - Indikator Sosial Ekonomi Se-Sumatera
+          merupakan kegiatan tahunan yang dilaksanakan dengan tujuan untuk
+          menyamakan visi dan misi dalam rangka pengambilan kebijakan
+          perencanaan pembangunan daerah di wilayah regional Sumatera
+        </p>
+        <p className="text-justify">
+          Provinsi Jambi menjadi tuan rumah dalam kegiatan Konreg PDRB-ISE
+          Se-Sumatera Tahun 2024. Penyelenggaraan kegiatan Konreg PDRB-ISE
+          Se-Sumatera Tahun 2024 dilaksanakan pada:
+        </p>
+        <table>
+          <tr>
+            <td className="py-4">Hari/Tanggal</td>
+            <td className="px-2">:</td>
+            <td>Senin s.d. Selasa, 26 - 27 Agustus 2024</td>
+          </tr>
+          <tr>
+            <td className="py-4">Tempat</td>
+            <td className="px-2">:</td>
+            <td>
+              <p>Hotel BW Luxury</p>
+              <p>
+                Jalan Soekarno-Hatta, Pasir Putih, Kecamatan Jambi Selatan, Kota
+                Jambi
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td className="py-4">Peserta</td>
+            <td className="px-2">:</td>
+            <td>
+              Bappeda Provinsi se-Sumatera, Dinas Komunikasi dan Informatika
+              se-Sumatera, Badan Pusat Statistik se-Sumatera, Kantor Perwakilan
+              Bank Indonesia Provinsi se-Sumatera, dan Kantor Wilayah DJPb
+              Provinsi se-Sumatera
+            </td>
+          </tr>
+        </table>
+        <p>
+          Adapun tema Konreg PDRB-ISE Se-Sumatera Tahun 2024 yaitu &quot;
+          <span className="font-bold">
+            Penguatan Transformasi Sosial Ekonomi Wilayah Sumatera Menuju
+            Akselerasi Pertumbuhan Ekonomi yang Inklusif dan Berkelanjutan
+          </span>
+          &quot;
         </p>
       </div>
     </div>
