3b403d98ad59df6a0269e26662684a1bb5343389
Thoriq Ibadurrohman <thoriq312@gmail.com>
2024-08-20 10:08:33 +0700

    fix transportasi and tentang

--- src/app/tentang/page.tsx
+++ src/app/tentang/page.tsx
@@ -11,32 +11,32 @@ const page = () => {
         <h1 className="font-bold text-xl lg:text-3xl text-sky-800">
           <Link href="/kontak">Tentang</Link>
         </h1>
-        <p className=" text-xs md:text-sm lg:text-base">
+        <p className="lg:text-base">
           Sekilas tentang Konreg PDRB-ISE Se-Sumatera 2024
         </p>
         <Image src="/assets/KONREG.png" alt="logo" width={700} height={100} />
-        <p className=" text-xs md:text-sm lg:text-base text-justify">
+        <p className="indent-8 text-justify">
           Kegiatan Konreg PDRB-ISE Se-Sumatera atau Kegiatan Konsultasi Regional
           Produk Domestik Regional Bruto - Indikator Sosial Ekonomi Se-Sumatera
           merupakan kegiatan tahunan yang dilaksanakan dengan tujuan untuk
           menyamakan visi dan misi dalam rangka pengambilan kebijakan
           perencanaan pembangunan daerah di wilayah regional Sumatera
         </p>
-        <p className="text-justify">
+        <p className="indent-8 text-justify">
           Provinsi Jambi menjadi tuan rumah dalam kegiatan Konreg PDRB-ISE
           Se-Sumatera Tahun 2024. Penyelenggaraan kegiatan Konreg PDRB-ISE
           Se-Sumatera Tahun 2024 dilaksanakan pada:
         </p>
         <table>
           <tr>
-            <td className="py-4">Hari/Tanggal</td>
+            <td>Hari/Tanggal</td>
             <td className="px-2">:</td>
-            <td>Senin s.d. Selasa, 26 - 27 Agustus 2024</td>
+            <td className="py-4">Senin s.d. Selasa, 26 - 27 Agustus 2024</td>
           </tr>
           <tr>
-            <td className="py-4">Tempat</td>
+            <td>Tempat</td>
             <td className="px-2">:</td>
-            <td>
+            <td className="py-4">
               <p>Hotel BW Luxury</p>
               <p>
                 Jalan Soekarno-Hatta, Pasir Putih, Kecamatan Jambi Selatan, Kota
@@ -45,9 +45,9 @@ const page = () => {
             </td>
           </tr>
           <tr>
-            <td className="py-4">Peserta</td>
+            <td>Peserta</td>
             <td className="px-2">:</td>
-            <td>
+            <td className="py-4">
               Bappeda Provinsi se-Sumatera, Dinas Komunikasi dan Informatika
               se-Sumatera, Badan Pusat Statistik se-Sumatera, Kantor Perwakilan
               Bank Indonesia Provinsi se-Sumatera, dan Kantor Wilayah DJPb
@@ -55,7 +55,7 @@ const page = () => {
             </td>
           </tr>
         </table>
-        <p>
+        <p className="indent-8 text-justify">
           Adapun tema Konreg PDRB-ISE Se-Sumatera Tahun 2024 yaitu &quot;
           <span className="font-bold">
             Penguatan Transformasi Sosial Ekonomi Wilayah Sumatera Menuju
