d68a279c1de24b86435dc00654ffa10ea03ba513
fikrianggara <fikriseptriananggara@gmail.com>
2024-08-27 10:44:23 +0700

    add galeri page

--- src/app/galeri/page.tsx
+++ src/app/galeri/page.tsx
@@ -1,11 +1,50 @@
+import Link from "next/link";
 import React from "react";
 
-const datas: string[] = [];
+const datas: any[] = [
+  {
+    id: 1,
+    title: "Penjemputan",
+    url: "https://license365bps-my.sharepoint.com/:f:/g/personal/aguswid_license365bps_onmicrosoft_com/EkekX6Zf8C1MmwT79b4R-EMBPKI2bXzHFq267wm3EYBv2Q?e=blMrlX",
+    thumbnail: "",
+  },
+  {
+    id: 2,
+    title: "High Level Meeting",
+    url: "https://license365bps-my.sharepoint.com/:f:/g/personal/aguswid_license365bps_onmicrosoft_com/EvboMmh0lNZHlhv3EEvBIjYBSemXUjSav9NcNbWqBdLxnA?e=BgUbhr",
+    thumbnail: "",
+  },
+  {
+    id: 3,
+    title: "Acara Pleno",
+    url: "https://license365bps-my.sharepoint.com/:f:/g/personal/aguswid_license365bps_onmicrosoft_com/EnxQMAaV60JNj6vC54qOksUBu1sj6dCmmkEZCdmDXewfmA?e=KCjQVF",
+    thumbnail: "",
+  },
+  {
+    id: 4,
+    title: "City Tour",
+    url: "https://home.s.id/#",
+    thumbnail: "",
+  },
+];
 const page = () => {
   return (
-    <div className="h-64 m-auto items-center text-center justify-center">
+    <div className="h-full flex flex-col m-auto items-center text-center justify-center space-y-6 mt-12">
+      <h1 className="text-xl md:text-4xl text-cyan-800 text-center font-medium">
+        Galeri
+      </h1>
       {datas.length > 0 ? (
-        datas.map((d, i) => <div key={i}>{d.toString()}</div>)
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-4 md:gap-4 w-8/12 self-center">
+          {datas.map((d, i) => (
+            <Link
+              href={d.url}
+              key={i}
+              className="h-24 w-full md:h-64 md:w-64 bg-cyan-50 flex items-center justify-center rounded-lg border rounded-lg transition-all duration-300 bg-gradient-to-tl from-indigo-600 from-10% via-sky-500 via-30% to-emerald-500 to-90% bg-size-200 bg-pos-0 hover:bg-pos-100"
+            >
+              <h2 className="md:text-xl text-white font-medium">{d.title}</h2>
+            </Link>
+          ))}
+        </div>
       ) : (
         <div className="h-full flex m-auto justify-center">
           <h1 className="text-xl text-gray-400 self-center">
